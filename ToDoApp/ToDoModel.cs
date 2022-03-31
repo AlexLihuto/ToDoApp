7d388eaf8bfc3db082f2808b10b2e10061655337
alex_ <alex_@LAPTOP-AOQUK0F8>
2022-03-31 10:44:45 +0300

    Добавьте файлы проекта.

---
+++ ToDoApp/ToDoModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp 
+{
+    class ToDoModel : INotifyPropertyChanged
+{
+        private string _ToDo;
+        private bool _IsDone;
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        public bool IsDone
+        {
+            get { return _IsDone; }
+            set 
+            {
+                if (_IsDone == value)
+                    return;
+                _IsDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        public string ToDo
+        {
+            get { return _ToDo; }
+            set 
+            {
+                if (_ToDo == value)
+                    return;
+                _ToDo = value;
+                OnPropertyChanged("ToDo");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
